ab94e4bf595aa43bedda812d0dc09de214914c53
Baldwin <cbaldwin@FVFZMYCQJ1WK.tld>
2022-04-12 14:54:17 -0500

    app.js progress

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+
 function run() {
-    getCard();
+
+    getCard()
 }
 
 function getCard() {
-    var r = Math.floor(Math.random() * 10) + 1
-    var suitArray = ["c", "D", "H", "S"]
+    var num = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + num + suitArray[randomSuit] + ".jpg>")
 }
 
 function randNum() {
@@ -17,38 +19,9 @@ function randNum() {
 function user(number) {
 
     var user = 0
-    
-    if (number == 1) {
-        var eleven = 11
-        number = eleven
-    }
 
-    user += (number)
-
-    if ((user > 20) && (eleven == 11)) {
-        user -= 10
-        eleven = 0
-    }
-
-    var suitArray = ["c", "D", "H", "S"]
-    var randomSuit = Math.floor(Math.random() * 4)
-    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + number + suitArray[randomSuit] + ".jpg>")
-
-    if (user > 20) {
-        break;
-    }
-
-    return user
-}
-
-function comp() {
-
-    var comp = 0
-
-    alert(`Now... it is computer's turn.`)
 
     do {
-
         var number = randNum()
 
         if (number == 1) {
@@ -56,38 +29,75 @@ function comp() {
             number = eleven
         }
 
-        comp += (number)
+        user += (number)
 
-        if ((comp > 20) && (eleven == 11)) {
-            comp -= 10
+        if ((user > 20) && (eleven == 11)) {
+            user -= 10
             eleven = 0
         }
 
-        alert(`The new number is ${number}.
-            
-        Computer Total: ${comp}.`)
-            
-    } while (comp < 16)
+        if (user > 20) {
+            break;
+        }
 
-    alert(`Computer will stop at ${comp}.`)
+    } while (document.getElementById('draw').clicked == true)
 
-    return comp
+    return user
 }
 
-function compare(user, comp) {
-    if (comp > 20) {
-        alert(`The computer went over 20! You won!`)
-    } else if (user > 20) {
-        alert(`You went over 20! The computer won!`)
-    } else if (user > comp) {
-        alert(`You won!`)
-    } else if (comp > user) {
-        alert(`The computer won!`)
-    } else {
-        alert(`You tied! No points awarded.`)
+    function comp() {
+
+        var comp = 0
+
+        do {
+
+            var number = randNum()
+
+            if (number == 1) {
+                var eleven = 11
+                number = eleven
+            }
+
+            comp += (number)
+
+            if ((comp > 20) && (eleven == 11)) {
+                comp -= 10
+                eleven = 0
+            }
+
+            alert(`The new number is ${number}.
+                
+            Computer Total: ${comp}.`)
+                
+        } while (comp < 16)
+
+        alert(`Computer will stop at ${comp}.`)
+
+        return comp
     }
 
-    var nothing = 0
+    function compare(user, comp) {
+        if (comp > 20) {
+            alert(`The computer went over 20! You won!`)
+        } else if (user > 20) {
+            alert(`You went over 20! The computer won!`)
+        } else if (user > comp) {
+            alert(`You won!`)
+        } else if (comp > user) {
+            alert(`The computer won!`)
+        } else {
+            alert(`You tied! No points awarded.`)
+        }
 
-    return nothing
+        var nothing = 0
+
+        return nothing
+}
+
+function master() {
+    do {
+        compare(user(), comp())
+        
+        var quit = prompt("Do you want to play again? Type 'n' if not.")
+    } while (quit != "n")
 }
